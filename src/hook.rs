90bf2b8dd6e1883c71e2ce6159bba68b0bd5a9a7
Oglo12 <jacksonnovak@proton.me>
2024-04-11 12:27:15 -0500

    Fixed display issue with hooks.

--- src/hook.rs
+++ src/hook.rs
@@ -6,13 +6,15 @@ macro_rules! run_hook_and_return_if_err {
     ) => {
         let macro_hook_path = crate::places::base_user().add_str("hooks").add_str(&format!("{}", $hook_name).replace(" ", "_"));
 
+        let hook_name = format!("{}", $hook_name).replace(" ", "_");
+
         if macro_hook_path.exists() {
-            crate::info!("Running hook: {}", $hook_name);
+            crate::info!("Running hook: {}", hook_name);
 
             match library::run_command(&macro_hook_path.to_string()) {
-                true => crate::info!("Successfully ran hook: {}", $hook_name),
+                true => crate::info!("Successfully ran hook: {}", hook_name),
                 false => {
-                    crate::error!("Failed to run hook: {}", $hook_name);
+                    crate::error!("Failed to run hook: {}", hook_name);
 
                     return Err(library::custom_error("Failed to run hook!"));
                 },
